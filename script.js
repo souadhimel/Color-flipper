c0cbba6d2d70592a94096277584ecca6db06eaab
Souad Himel <souad.anam@gmail.com>
2021-09-08 19:40:49 +0600

    text

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = `rgb(
+      ${Math.floor(Math.random() * 256)},
+      ${Math.floor(Math.random() * 256)},
+      ${Math.floor(Math.random() * 256)})`;
+});
