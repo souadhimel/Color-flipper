866165d8d1da1a17ba697a0545b831a880017ef1
Souad Himel <souad.anam@gmail.com>
2021-09-15 19:51:57 +0600

    text

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ btn.addEventListener("click", () => {
       ${Math.floor(Math.random() * 256)},
       ${Math.floor(Math.random() * 256)})`;
 });
+Math.random will give you a floating point number between 0 (inclusive) and 1 (exclusive).
+
+Multiplying that by 256 will give you a number in the range 0 (inclusive) through 256 (exclusive), but still floating point.
+
+Taking the floor of that number will give you an integer between 0 and 255 (both inclusive).
+
+It's the integer from 0 to 255 that you need to construct RGB values like rgb(72,25,183).
